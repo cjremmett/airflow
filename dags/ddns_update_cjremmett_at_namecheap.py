0b38de0f2167d9f10d78f8fdc1eebd9bca124449
boggledstarsector <boggledstarsector@gmail.com>
2025-03-13 19:57:45 -0400

    add port to API call

--- dags/ddns_update_cjremmett_at_namecheap.py
+++ dags/ddns_update_cjremmett_at_namecheap.py
@@ -8,8 +8,8 @@ def ddns_update_cjremmett_at_namecheap():
     token = Variable.get("ddns_token")
     domain_name = 'cjremmett.com'
     host = '@'
-    # Need to use the internal IP of the server because if the public IP changed cjremmett.com will be unreachable
-    internal_ip = '192.168.0.121'
+    # Need to use the internal IP of the flask backend because if the public IP changed cjremmett.com will be unreachable
+    internal_ip = '192.168.0.121:5000'
     response = requests.put(f'http://{internal_ip}/flask/dynamic-dns/update-namecheap-dns-record?host={host}&domain_name={domain_name}', headers={"token": token})
     if response.status_code > 201:
         raise Exception('API call failed. Status code: ' + str(response.status_code))
